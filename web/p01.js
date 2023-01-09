a1a2e94e16d57c9682fbf3c0390f2d3881fae9a4
LIN-PO-CHIH <chih910121@gmail.com>
2023-01-09 23:58:17 +0800

    Update p01.js

--- web/p01.js
+++ web/p01.js
@@ -15,13 +15,15 @@ function GetH(){
     return h;
 }
 function towork(){
+    console.log(GetH());
+    console.log(checkinH);
     if(!workstate & (GetH()-checkinH >= 1)){
         geoFindMe();
         document.getElementById("checktime").textContent = "打卡時間(上班)" + GetTime();
         checkinH = GetH();
         workstate = true;
     }else if(GetH()-checkinH < 1){
-        window.alert("錯誤的輸入\n上班時間不可低於一小時！");
+        window.alert("錯誤的輸入\n休息時間不可低於一小時！");
     }else{
         window.alert("你目前正在上班\n請先打卡下班後再次嘗試！");
     }
@@ -67,4 +69,4 @@ var call2 = document.getElementById('act2');
 call1?.addEventListener("click", towork);
 call2?.addEventListener("click", offwork);
 var workstate = false;
-var checkinH = 0;
+var checkinH = -1;
